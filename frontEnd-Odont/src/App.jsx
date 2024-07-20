8107dc6196b0f8b9d2178803679c6f7c5b5800ed
AndrewGlez <andrewgonzale009@gmail.com>
2024-07-19 21:36:32 -0500

    register fichas

--- frontEnd-Odont/src/App.jsx
+++ frontEnd-Odont/src/App.jsx
@@ -3,24 +3,28 @@ import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
-import './App.css'
+import "./App.css";
 import { LoginPage } from "./Pages/LoginPage";
-import { MainPage } from "./Pages/MainPage";
 
+import NavBar from "./Components/NavBar";
+import MainPage from "./Pages/MainPage";
+import FichaPage from "./Pages/FichaPage";
 
 function App() {
-  
   return (
     <>
       <BrowserRouter>
+        <NavBar />
+
         <Routes>
           <Route path="/" element={<LoginPage />}></Route>
-          <Route path="/index" element={<MainPage/>}></Route>
-          
+          <Route path="/index" element={<MainPage />}></Route>
+          <Route path="/registrar-ficha" element={<FichaPage />}></Route>
+          <Route path="/ver-ficha" element={<FichaPage />}></Route>
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
